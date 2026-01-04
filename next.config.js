bbe349e27ec60fd8d10ed4d81bad8edd57e2745b
doruksigma <dorukulusoyps@gmail.com>
2026-01-04 23:26:33 +0300

    Update next.config.js
    Signed-off-by: doruksigma <dorukulusoyps@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.worker\.(js|ts)$/,
-      use: { loader: "worker-loader" },
-    });
-    return config;
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
