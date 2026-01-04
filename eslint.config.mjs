3148c44fe116ab8ce7444700489534dc3bb255d7
doruksigma <dorukulusoyps@gmail.com>
2026-01-04 23:22:36 +0300

    Update eslint.config.mjs
    Signed-off-by: doruksigma <dorukulusoyps@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,12 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
-
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+ xarmy@0.1.0 build
+> next build
+   ▲ Next.js 15.5.9
+   Creating an optimized production build ...
+ ✓ Compiled successfully in 11.2s
+   Linting and checking validity of types ...
+ ⨯ ESLint: nextVitals is not iterable
+Failed to compile.
+./app/games/chess/engine.worker.ts:2:23
+Type error: Cannot find module 'stockfish' or its corresponding type declarations.
+  1 | // app/games/chess/engine.worker.ts
+> 2 | import Stockfish from "stockfish";
