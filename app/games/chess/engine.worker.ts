d1edb874929d9d30d430cfcd72d82387c801c854
doruksigma <dorukulusoyps@gmail.com>
2026-01-04 23:23:10 +0300

    Update engine.worker.ts
    Signed-off-by: doruksigma <dorukulusoyps@gmail.com>

--- app/games/chess/engine.worker.ts
+++ app/games/chess/engine.worker.ts
@@ -1,16 +1,16 @@
 // app/games/chess/engine.worker.ts
-import Stockfish from "stockfish";
 
-// stockfish() worker-like bir nesne döndürür: postMessage/onmessage
+// TypeScript type derdini bitirmek için require kullanıyoruz
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Stockfish = require("stockfish");
+
 const sf: any = Stockfish();
 
-// Stockfish -> UI
 sf.onmessage = (e: any) => {
   const line = typeof e === "string" ? e : e?.data;
   (self as any).postMessage(line);
 };
 
-// UI -> Stockfish
 (self as any).onmessage = (e: MessageEvent) => {
   sf.postMessage(e.data);
 };
