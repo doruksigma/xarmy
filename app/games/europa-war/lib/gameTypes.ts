cf58c88d432cba71d55b11bc45c3d5ac291df01d
doruksigma <dorukulusoyps@gmail.com>
2026-04-03 17:09:29 +0300

    Update gameTypes.ts
    Signed-off-by: doruksigma <dorukulusoyps@gmail.com>

--- app/games/europa-war/lib/gameTypes.ts
+++ app/games/europa-war/lib/gameTypes.ts
@@ -1,1 +1,44 @@
 export type UnitType = "infantry" | "tank" | "aircraft";
+
+export type CountryId =
+  | "turkey"
+  | "greece"
+  | "bulgaria"
+  | "romania"
+  | "serbia"
+  | "hungary"
+  | "germany"
+  | "france"
+  | "italy"
+  | "spain";
+
+export interface Unit {
+  id: string;
+  type: UnitType;
+  attack: number;
+  defense: number;
+  moveDays: number;
+  cost: number;
+  manpowerCost: number;
+  ownerCountryId: CountryId;
+}
+
+export interface City {
+  id: string;
+  name: string;
+  countryId: CountryId;
+  ownerCountryId: CountryId;
+  x: number;
+  y: number;
+  economy: number;
+  factories: number;
+  stationedUnits: Unit[];
+}
+
+export interface Country {
+  id: CountryId;
+  name: string;
+  color: string;
+  treasury: number;
+  factories: number;
+}
